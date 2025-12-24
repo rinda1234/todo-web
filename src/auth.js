7ca90512caa2c73f100edd63ded63a2f150211bb
이대복 <daebok6962@gmail.com>
2025-12-24 17:34:55 +0900

    add login

---
+++ src/auth.js
@@ -0,0 +1,7 @@
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "./firebase";
+
+const provider = new GoogleAuthProvider();
+
+export const loginWithGoogle = () => signInWithPopup(auth, provider);
+export const logout = () => signOut(auth);

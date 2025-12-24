a8a5bd59fe4ae50a852bcf46832f9c344e2e0b28
이대복 <daebok6962@gmail.com>
2025-12-24 17:43:05 +0900

    add main page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,4 @@
-import { loginWithGoogle, loginWithGithub, logout } from "./auth";
+import { loginWithGoogle, loginWithGithub } from "./auth";
 
 export default function Login() {
     return (
@@ -6,7 +6,7 @@ export default function Login() {
             <h1>TODO Web</h1>
             <button onClick={loginWithGoogle}>Google로 로그인</button>
             <button onClick={loginWithGithub}>Github로 로그인</button>
-            <button onClick={logout}>로그아웃</button>
+
         </div>
     );
 }
